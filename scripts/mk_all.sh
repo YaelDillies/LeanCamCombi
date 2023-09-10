fc3cbf258fd87244352cf30736e34c614ed3924c
Yaël Dillies <yael.dillies@gmail.com>
2023-09-10 16:17:50 +0000

    mk_all

--- scripts/mk_all.sh
+++ scripts/mk_all.sh
@@ -4,17 +4,23 @@
 # Examples:
 #   ./scripts/mk_all.sh
 #   ./scripts/mk_all.sh data/real
+#   ./scripts/mk_all.sh ../archive
 #
 # Makes a mathlib/src/$directory/all.lean importing all files inside $directory.
 # If $directory is omitted, creates `mathlib/src/all.lean`.
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"/../src
+cd "$( dirname "${BASH_SOURCE[0]}" )"/../LeanCamCombi
 if [[ $# = 1 ]]; then
-  dir="$1"
+  dir="${1%/}"  # remove trailing slash if present
 else
   dir="."
 fi
 
-find "$dir" -name \*.lean -not -name all.lean \
-  | sed 's,^\./,,;s,\.lean$,,;s,/,.,g;s,^,import ,' \
-  | sort >"$dir"/all.lean
+# remove an initial `./`
+# replace an initial `../test/` with just `.` (similarly for `roadmap`/`archive`/...)
+# replace all `/` with `».«`
+# replace the `.lean` suffix with `»`
+# prepend `import «`
+find "$dir" -name \*.lean -not -name LeanCamCombi.lean \
+  | sed 's,^\./,,;s,^\.\./[^/]*,,;s,/,.,g;s,\.lean$,,;s,^,import LeanCamCombi.,' \
+  | sort >"$dir"/../LeanCamCombi.lean
