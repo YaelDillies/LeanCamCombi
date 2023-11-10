f3efeba52355a6f3f3481aa6d388f7d790cb13e4
Yaël Dillies <yael.dillies@gmail.com>
2023-11-10 15:57:50 +0000

    cleanup squarefree

--- LeanCamCombi/Mathlib/Data/Nat/Order/Lemmas.lean
+++ LeanCamCombi/Mathlib/Data/Nat/Order/Lemmas.lean
@@ -3,7 +3,10 @@ import Mathlib.Data.Nat.Order.Lemmas
 namespace Nat
 variable {a b : ℕ}
 
-protected lemma div_ne_zero (hb : b ≠ 0) : a / b ≠ 0 ↔ b ≤ a := by
+protected lemma div_ne_zero_iff (hb : b ≠ 0) : a / b ≠ 0 ↔ b ≤ a := by
   rw [ne_eq, Nat.div_eq_zero_iff hb.bot_lt, not_lt]
+
+protected lemma div_pos_iff (hb : b ≠ 0) : 0 < a / b ↔ b ≤ a := by
+  rw [pos_iff_ne_zero, Nat.div_ne_zero_iff hb]
 
 end Nat
