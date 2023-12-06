5ee6741d1d11e2df6f801967528e354cc237fb91
Yaël Dillies <yael.dillies@gmail.com>
2023-12-06 16:29:18 +0000

    Distribute inverse over pointwise scalar multiplication

--- LeanCamCombi/Mathlib/Data/Finset/Pointwise.lean
+++ LeanCamCombi/Mathlib/Data/Finset/Pointwise.lean
@@ -3,6 +3,8 @@ import Mathlib.Data.Finset.Pointwise
 open MulOpposite
 open scoped Pointwise
 
+attribute [simp] Finset.zero_smul_finset
+
 variable {α : Type*} {s : Set ℕ}
 
 instance Nat.decidablePred_mem_vadd [DecidablePred (· ∈ s)] (a : ℕ) : DecidablePred (· ∈ a +ᵥ s) :=
@@ -28,4 +30,42 @@ lemma singleton_mul' (a : α) (s : Finset α) : {a} * s = a • s := singleton_m
 lemma mul_singleton' (s : Finset α) (a : α) : s * {a} = op a • s := mul_singleton _
 
 end Mul
+
+section InvolutiveInv
+variable [DecidableEq α] [InvolutiveInv α] {s : Finset α} {a : α}
+
+@[to_additive (attr := simp)]
+lemma mem_inv' : a ∈ s⁻¹ ↔ a⁻¹ ∈ s := by simp [mem_inv, inv_eq_iff_eq_inv]
+
+end InvolutiveInv
+
+section Group
+variable [DecidableEq α] [Group α]
+
+@[to_additive (attr := simp)]
+lemma inv_smul_finset_distrib (a : α) (s : Finset α) : (a • s)⁻¹ = op a⁻¹ • s⁻¹ := by
+  ext; simp [← inv_smul_mem_iff]
+
+@[to_additive (attr := simp)]
+lemma inv_op_smul_finset_distrib (a : α) (s : Finset α) : (op a • s)⁻¹ = a⁻¹ • s⁻¹ := by
+  ext; simp [← inv_smul_mem_iff]
+
+end Group
+
+section GroupWithZero
+variable [DecidableEq α] [GroupWithZero α]
+
+@[simp] protected lemma inv_zero : (0 : Finset α)⁻¹ = 0 := by ext; simp
+
+@[simp] lemma inv_smul_set_distrib₀ (a : α) (s : Finset α) : (a • s)⁻¹ = op a⁻¹ • s⁻¹ := by
+  obtain rfl | ha := eq_or_ne a 0
+  · obtain rfl | hs := s.eq_empty_or_nonempty <;> simp [*]
+  · ext; simp [← inv_smul_mem_iff₀, *]
+
+@[simp] lemma inv_op_smul_set_distrib₀ (a : α) (s : Finset α) : (op a • s)⁻¹ = a⁻¹ • s⁻¹ := by
+  obtain rfl | ha := eq_or_ne a 0
+  · obtain rfl | hs := s.eq_empty_or_nonempty <;> simp [*]
+  · ext; simp [← inv_smul_mem_iff₀, *]
+
+end GroupWithZero
 end Finset
--- LeanCamCombi/Mathlib/Data/Set/Pointwise/SMul.lean
+++ LeanCamCombi/Mathlib/Data/Set/Pointwise/SMul.lean
@@ -1,19 +1,51 @@
 import Mathlib.Data.Set.Pointwise.SMul
 
 open MulOpposite
-
 open scoped Pointwise
 
+attribute [simp] Set.zero_smul_set
+
 namespace Set
+variable {α : Type*}
 
-variable {α : Type*} [Mul α]
-
-@[to_additive (attr := simp)]
-lemma singleton_mul' (a : α) (s : Set α) : {a} * s = a • s :=
-  singleton_mul
+section Mul
+variable [Mul α]
 
 @[to_additive (attr := simp)]
-lemma mul_singleton' (s : Set α) (a : α) : s * {a} = op a • s :=
-  mul_singleton
+lemma singleton_mul' (a : α) (s : Set α) : {a} * s = a • s := singleton_mul
 
+@[to_additive (attr := simp)]
+lemma mul_singleton' (s : Set α) (a : α) : s * {a} = op a • s := mul_singleton
+
+end Mul
+
+section Group
+variable [Group α]
+
+@[to_additive (attr := simp)]
+lemma inv_smul_set_distrib (a : α) (s : Set α) : (a • s)⁻¹ = op a⁻¹ • s⁻¹ := by
+  ext; simp [mem_smul_set_iff_inv_smul_mem]
+
+@[to_additive (attr := simp)]
+lemma inv_op_smul_set_distrib (a : α) (s : Set α) : (op a • s)⁻¹ = a⁻¹ • s⁻¹ := by
+  ext; simp [mem_smul_set_iff_inv_smul_mem]
+
+end Group
+
+section GroupWithZero
+variable [GroupWithZero α]
+
+@[simp] protected lemma inv_zero : (0 : Set α)⁻¹ = 0 := by ext; simp
+
+@[simp] lemma inv_smul_set_distrib₀ (a : α) (s : Set α) : (a • s)⁻¹ = op a⁻¹ • s⁻¹ := by
+  obtain rfl | ha := eq_or_ne a 0
+  · obtain rfl | hs := s.eq_empty_or_nonempty <;> simp [*]
+  · ext; simp [mem_smul_set_iff_inv_smul_mem₀, *]
+
+@[simp] lemma inv_op_smul_set_distrib₀ (a : α) (s : Set α) : (op a • s)⁻¹ = a⁻¹ • s⁻¹ := by
+  obtain rfl | ha := eq_or_ne a 0
+  · obtain rfl | hs := s.eq_empty_or_nonempty <;> simp [*]
+  · ext; simp [mem_smul_set_iff_inv_smul_mem₀, *]
+
+end GroupWithZero
 end Set
