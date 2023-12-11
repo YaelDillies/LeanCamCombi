475375f41749f9ec5f2363776323f90e424b5549
Yaël Dillies <yael.dillies@gmail.com>
2023-12-11 13:45:00 +0100

    `Finset.Nontrivial.exists_cons_eq`

--- LeanCamCombi/Mathlib/Data/Finset/Basic.lean
+++ LeanCamCombi/Mathlib/Data/Finset/Basic.lean
@@ -1,8 +1,22 @@
 import Mathlib.Data.Finset.Basic
 
-variable {α : Type*} [DecidableEq α] {s t : Finset α} {a b : α}
-
 namespace Finset
+variable {α : Type*} {s t : Finset α} {a b : α}
+
+lemma Nonempty.exists_cons_eq (hs : s.Nonempty) : ∃ t a ha, cons a t ha = s := by
+  classical
+  obtain ⟨a, ha⟩ := hs
+  exact ⟨s.erase a, a, not_mem_erase _ _, by simp [insert_erase ha]⟩
+
+lemma Nontrivial.exists_cons_eq (hs : s.Nontrivial) :
+    ∃ t a ha b hb hab, (cons b t hb).cons a (mem_cons.not.2 $ not_or_intro hab ha) = s := by
+  classical
+  obtain ⟨a, ha, b, hb, hab⟩ := hs
+  have : b ∈ s.erase a := mem_erase.2 ⟨hab.symm, hb⟩
+  refine ⟨(s.erase a).erase b, a, ?_, b, ?_, ?_, ?_⟩ <;>
+    simp [insert_erase this, insert_erase ha, *]
+
+variable [DecidableEq α]
 
 protected alias ⟨_, Nonempty.attach⟩ := attach_nonempty_iff
 
