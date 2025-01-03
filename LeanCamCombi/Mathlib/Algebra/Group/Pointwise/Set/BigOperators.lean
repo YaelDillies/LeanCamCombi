a53778b9780143222102d65b6cb7ab616365ba92
Yaël Dillies <yael.dillies@gmail.com>
2025-01-03 16:02:36 +0000

    Prove `Set.mem_pow_iff_prod`

--- LeanCamCombi/Mathlib/Algebra/Group/Pointwise/Set/BigOperators.lean
+++ LeanCamCombi/Mathlib/Algebra/Group/Pointwise/Set/BigOperators.lean
@@ -1,5 +1,5 @@
-import Mathlib.Algebra.Group.Pointwise.Set.BigOperators
-import Mathlib.Algebra.Group.Pointwise.Set.ListOfFn
+import Mathlib.Algebra.BigOperators.Fin
+import Mathlib.Algebra.Group.Pointwise.Set.Basic
 
 open scoped Pointwise
 
@@ -7,14 +7,22 @@ namespace Set
 variable {α : Type*}
 
 section CommMonoid
-variable [CommMonoid α] {s t : Set α} {a : α} {n : ℕ}
+variable [CommMonoid α] {n : ℕ}
 
 @[to_additive]
-lemma mem_pow_iff_prod : ∀ {n}, a ∈ s ^ n ↔ ∃ f : Fin n → α, (∀ i, f i ∈ s) ∧ ∏ i, f i = a
-  | 0 => by simp [eq_comm]
-  | n + 1 => by
-    simp_rw [pow_succ, mem_mul]
-    sorry
+lemma mem_pow_iff_prod :
+    ∀ {n} {s : Set α} {a : α}, a ∈ s ^ n ↔ ∃ f : Fin n → α, (∀ i, f i ∈ s) ∧ ∏ i, f i = a
+  | 0, s, a => by simp [eq_comm]
+  | n + 1, s, a => by
+    rw [(Fin.consEquiv _).symm.exists_congr_left]
+    simp only [pow_succ, mem_mul, mem_pow_iff_prod, exists_exists_and_eq_and, Equiv.symm_symm,
+      Fin.consEquiv_apply, Fin.forall_iff_succ, Fin.cons_zero, Fin.cons_succ, Prod.exists,
+      Fin.prod_univ_succ]
+    constructor
+    · rintro ⟨f, hf, b, hb, rfl⟩
+      exact ⟨b, f, ⟨hb, hf⟩, mul_comm ..⟩
+    · rintro ⟨b, f, ⟨hb, hf⟩, rfl⟩
+      exact ⟨f, hf, b, hb, mul_comm ..⟩
 
 end CommMonoid
 end Set
