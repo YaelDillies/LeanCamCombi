08e3d72dd4289456c494052cf77ff26cd5f3db4c
Yaël Dillies <yael.dillies@gmail.com>
2025-01-17 10:14:41 +0000

    Bumping script for the new doc-gen setup

--- scripts/update_mathlib.sh
+++
@@ -1,4 +0,0 @@
-# Update mathlib and the lean toolchain.
-curl -L https://raw.githubusercontent.com/leanprover-community/mathlib4/master/lean-toolchain -o lean-toolchain
-lake -R -Kenv=dev update
-lake exe cache get
