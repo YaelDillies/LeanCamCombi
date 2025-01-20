e6ab073907ed89c71110667fc25cf77613097893
Yaël Dillies <yael.dillies@gmail.com>
2025-01-20 22:27:58 +0000

    remove sorry

--- LeanCamCombi/Mathlib/MeasureTheory/Function/ConditionalExpectation/Basic.lean
+++ LeanCamCombi/Mathlib/MeasureTheory/Function/ConditionalExpectation/Basic.lean
@@ -24,42 +24,6 @@ attribute [simp] condexp_const
 
 alias condexp_of_not_integrable := condexp_undef
 
-variable [IsFiniteMeasure μ] [InnerProductSpace ℝ E]
-
-lemma Memℒp.condexpL2_eq_condexp (hm : m ≤ m₀) (hf : Memℒp f 2 μ) :
-    (condexpL2 E ℝ hm (hf.toLp _) : Ω → E) =ᵐ[μ] μ[f | m] := by
-  refine ae_eq_condexp_of_forall_setIntegral_eq hm
-    (memℒp_one_iff_integrable.1 <| hf.mono_exponent one_le_two) ?_ ?_ ?_
-  · intros s hs htop
-    exact integrableOn_condexpL2_of_measure_ne_top hm htop.ne _
-  · intros s hs htop
-    rw [integral_condexpL2_eq hm (hf.toLp _) hs htop.ne]
-    refine setIntegral_congr_ae (hm _ hs) ?_
-    filter_upwards [hf.coeFn_toLp] with ω hω _ using hω
-  · exact aeStronglyMeasurable'_condexpL2 hm _
-
-lemma eLpNorm_condexp_le' (hm : m ≤ m₀) (hf : Memℒp f 2 μ) :
-    eLpNorm (μ[f | m]) 2 μ ≤ eLpNorm f 2 μ := by
-  rw [← eLpNorm_congr_ae (hf.condexpL2_eq_condexp hm)]
-  refine le_trans (eLpNorm_condexpL2_le hm _) ?_
-  rw [eLpNorm_congr_ae hf.coeFn_toLp]
-
-lemma eLpNorm_condexp_le : eLpNorm (μ[f | m]) 2 μ ≤ eLpNorm f 2 μ := by
-  by_cases hm : m ≤ m₀
-  swap; simp [condexp_of_not_le hm]
-  by_cases hfm : AEStronglyMeasurable f μ
-  swap
-  rw [condexp_undef (by simp [Integrable, not_and_of_not_left _ hfm])]
-  · simp
-  by_cases hf : eLpNorm f 2 μ = ∞; simp [hf]
-  refine eLpNorm_condexp_le' hm ⟨hfm, Ne.lt_top' fun a ↦ hf (id (Eq.symm a))⟩
-
-protected lemma Memℒp.condexp (hf : Memℒp f 2 μ) : Memℒp (μ[f | m]) 2 μ := by
-  by_cases hm : m ≤ m₀
-  · exact ⟨(stronglyMeasurable_condexp.mono hm).aestronglyMeasurable,
-      eLpNorm_condexp_le.trans_lt hf.eLpNorm_lt_top⟩
-  · simp [condexp_of_not_le hm]
-
 section NormedRing
 variable [NormedRing R] [NormedSpace ℝ R] [CompleteSpace R] {f g : Ω → R}
 
@@ -69,9 +33,33 @@ lemma condexp_ofNat (n : ℕ) [n.AtLeastTwo] (f : Ω → R) :
 
 end NormedRing
 
-section Real
-variable {f : Ω → ℝ}
+variable [IsFiniteMeasure μ] [InnerProductSpace ℝ E]
 
-lemma integral_condexp_sq_le : ∫ ω, (μ[f | m]) ω ^ 2 ∂μ ≤ ∫ ω, f ω ^ 2 ∂μ := sorry
+lemma Memℒp.condexpL2_ae_eq_condexp (hm : m ≤ m₀) (hf : Memℒp f 2 μ) :
+    condexpL2 E ℝ hm hf.toLp =ᵐ[μ] μ[f | m] := by
+  refine ae_eq_condexp_of_forall_setIntegral_eq hm
+    (memℒp_one_iff_integrable.1 <| hf.mono_exponent one_le_two)
+    (fun s hs htop ↦ integrableOn_condexpL2_of_measure_ne_top hm htop.ne _) (fun s hs htop ↦ ?_)
+    (aeStronglyMeasurable'_condexpL2 hm _)
+  rw [integral_condexpL2_eq hm (hf.toLp _) hs htop.ne]
+  refine setIntegral_congr_ae (hm _ hs) ?_
+  filter_upwards [hf.coeFn_toLp] with ω hω _ using hω
 
-end Real
+lemma eLpNorm_condexp_le : eLpNorm (μ[f | m]) 2 μ ≤ eLpNorm f 2 μ := by
+  by_cases hm : m ≤ m₀; swap
+  · simp [condexp_of_not_le hm]
+  by_cases hfm : AEStronglyMeasurable f μ; swap
+  · rw [condexp_undef (by simp [Integrable, not_and_of_not_left _ hfm])]
+    simp
+  obtain hf | hf := eq_or_ne (eLpNorm f 2 μ) ∞
+  · simp [hf]
+  replace hf : Memℒp f 2 μ := ⟨hfm, Ne.lt_top' fun a ↦ hf (id (Eq.symm a))⟩
+  rw [← eLpNorm_congr_ae (hf.condexpL2_ae_eq_condexp hm)]
+  refine le_trans (eLpNorm_condexpL2_le hm _) ?_
+  rw [eLpNorm_congr_ae hf.coeFn_toLp]
+
+protected lemma Memℒp.condexp (hf : Memℒp f 2 μ) : Memℒp (μ[f | m]) 2 μ := by
+  by_cases hm : m ≤ m₀
+  · exact ⟨(stronglyMeasurable_condexp.mono hm).aestronglyMeasurable,
+      eLpNorm_condexp_le.trans_lt hf.eLpNorm_lt_top⟩
+  · simp [condexp_of_not_le hm]
