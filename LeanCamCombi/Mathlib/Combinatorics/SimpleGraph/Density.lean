d35d438122c975acede5485b8a5d4fd4c2f1fd2c
Yaël Dillies <yael.dillies@gmail.com>
2025-03-15 13:02:32 +0000

    Make edge densities `ℚ≥0`-valued

--- LeanCamCombi/Mathlib/Combinatorics/SimpleGraph/Density.lean
+++ LeanCamCombi/Mathlib/Combinatorics/SimpleGraph/Density.lean
@@ -1,5 +1,11 @@
-import Mathlib.Algebra.Order.Ring.Unbundled.Rat
+import Mathlib.Algebra.Field.Rat
 import Mathlib.Combinatorics.SimpleGraph.Subgraph
+
+/-!
+# TODO
+
+Rename `edgeDensity` to `edgeDensityBtw`
+-/
 
 open Finset
 open scoped Classical
@@ -7,9 +13,13 @@ open scoped Classical
 namespace SimpleGraph
 variable {α : Type*} [Fintype α] (G : SimpleGraph α) [Fintype G.edgeSet]
 
-def fullEdgeDensity : ℚ := #G.edgeFinset / Fintype.card α
+/-- The edge density of a simple graph is its number of edges divided by its number of vertices.
 
-noncomputable def maxEdgeSubdensity : ℚ :=
-  Finset.univ.sup' Finset.univ_nonempty fun G' : G.Subgraph ↦ G'.coe.fullEdgeDensity
+In other words, it is half of its average degree. -/
+def edgeDensity' : ℚ≥0 := #G.edgeFinset / Fintype.card α
+
+/-- The maximum edge density of a subgraph of a graph. -/
+noncomputable def maxEdgeSubdensity : ℚ≥0 :=
+  Finset.univ.sup' Finset.univ_nonempty fun G' : G.Subgraph ↦ G'.coe.edgeDensity'
 
 end SimpleGraph
