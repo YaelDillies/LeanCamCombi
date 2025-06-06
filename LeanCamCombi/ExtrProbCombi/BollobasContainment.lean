6ac2c45aba1b06927e713366ace8624400e0626b
Yaël Dillies <yael.dillies@gmail.com>
2025-06-06 08:29:04 +0000

    Bump mathlib to v4.20.1

--- LeanCamCombi/ExtrProbCombi/BollobasContainment.lean
+++ LeanCamCombi/ExtrProbCombi/BollobasContainment.lean
@@ -3,11 +3,11 @@ Copyright (c) 2023 Yaël Dillies. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yaël Dillies
 -/
+import LeanCamCombi.ExtrProbCombi.BinomialRandomGraph
+import LeanCamCombi.Mathlib.Combinatorics.SimpleGraph.Density
 import Mathlib.Analysis.SpecialFunctions.Pow.Real
 import Mathlib.Combinatorics.SimpleGraph.Copy
-import Mathlib.MeasureTheory.Measure.Typeclasses
-import LeanCamCombi.Mathlib.Combinatorics.SimpleGraph.Density
-import LeanCamCombi.ExtrProbCombi.BinomialRandomGraph
+import Mathlib.MeasureTheory.Measure.Typeclasses.Probability
 
 /-!
 # Bollobás' graph containment lemma
